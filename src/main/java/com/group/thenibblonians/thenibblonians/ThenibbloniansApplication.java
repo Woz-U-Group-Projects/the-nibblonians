c730bd53a62a71d6b5ebebb72e654c507cf312d8
samchen6 <samantha.willcott@my.scitexas.edu>
2019-07-03 13:42:37 -0400

    second commit

---
+++ src/main/java/com/group/thenibblonians/thenibblonians/ThenibbloniansApplication.java
@@ -0,0 +1,13 @@
+package com.group.thenibblonians.thenibblonians;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThenibbloniansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThenibbloniansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theNibblonians/theNibblonians/TheNibbloniansApplicationTests.java
@@ -0,0 +1,16 @@
+package com.theNibblonians.theNibblonians;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheNibbloniansApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
