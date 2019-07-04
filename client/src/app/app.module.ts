29013d24e7f6246f52291651abb9d3f287efd34b
Blayne Bennett <blaynebennett99@gmail.com>
2019-07-03 17:18:34 -0700

    no changes still; install deps

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
