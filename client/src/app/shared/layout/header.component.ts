f44f7c5a47b217fb528ebd358ab7f506c51298af
Blayne Bennett <blaynebennett99@gmail.com>
2019-07-15 13:25:31 -0700

    nav update

--- client/src/app/shared/layout/header.component.ts
+++ client/src/app/shared/layout/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'layout-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  isShown:boolean = false;
   constructor() {}
+
+  ngOnInit() {
+  }
 
 }
