459a19563699ff568a752b2eb1b8dbab8ab34e18
sam <samantha.willcott@my.scitexas.edu>
2019-07-22 15:22:23 -0400

    home

---
+++ Desktop/NB/the-nibblonians/client/src/continue/continue.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-continue',
+  templateUrl: './continue.component.html',
+  styleUrls: ['./continue.component.css']
+})
+export class ContinueComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
